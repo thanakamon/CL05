072d71ffbbd891755205daf7be5342f8c7e78010
thanakamon <thanakamonmay@gmail.com>
2018-10-02 01:07:57 +0700

    22

--- CL05/CL05/Program.cs
+++ CL05/CL05/Program.cs
@@ -8,9 +8,14 @@ namespace ConsoleApp2
         {
             for (int i = 1; i <= 12; i++)
             {
-                Console.WriteLine("{0, 2} x 2 = {1, 2}", i, i * 2);
+                for (int j = 1; j <= 12; j++)
+                {
+                    Console.Write("{0, 4}", i * j);
+                }
+                Console.WriteLine();
             }
         }
     }
 }
+
 
