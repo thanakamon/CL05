d2dd36227e076d5669df8c1db7951eae561ce472
thanakamon <thanakamonmay@gmail.com>
2018-10-02 01:10:30 +0700

    23

--- CL05/CL05/Program.cs
+++ CL05/CL05/Program.cs
@@ -2,20 +2,23 @@
 
 namespace ConsoleApp2
 {
-    class Multiply
+    class BreakTest
     {
         static void Main()
         {
-            for (int i = 1; i <= 12; i++)
+            for (int i = 1; i <= 100; i++)
             {
-                for (int j = 1; j <= 12; j++)
+                if (i == 5)
                 {
-                    Console.Write("{0, 4}", i * j);
+                    break;
                 }
-                Console.WriteLine();
+                Console.WriteLine(i);
             }
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
-
 
