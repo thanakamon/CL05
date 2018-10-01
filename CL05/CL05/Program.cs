5d46af2a90b52435455a396cbb24580dd3c35e4c
thanakamon <thanakamonmay@gmail.com>
2018-10-01 23:48:36 +0700

    15

--- CL05/CL05/Program.cs
+++ CL05/CL05/Program.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 public class IfLearning
 {
     public static void Main()
     {
-        int a = 2;
-        if (a == 2)
+        int a = 10;
+        int b = 20;
+        if (a == 10)
+            
         {
-            Console.WriteLine("execute this line");
+            if (b == 20)
+            {
+
+                System.Console.WriteLine("a = 10 and b = 20");
+            }
+            if (b != 20)
+            {
+                System.Console.WriteLine("a = 10 and b != 20");
+            }
         }
-        else
-        {
-            Console.WriteLine("execute another line too");
-        }
-        Console.WriteLine("this line is always execute");
     }
 }
+
+
 
 
 
