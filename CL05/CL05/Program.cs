71424f19809a43a91299d1023dc25d306c873146
thanakamon <thanakamonmay@gmail.com>
2018-10-02 01:17:15 +0700

    25

--- CL05/CL05/Program.cs
+++ CL05/CL05/Program.cs
@@ -2,17 +2,28 @@
 
 namespace ConsoleApp2
 {
-    class BreakTest
+    class Switch
     {
         static void Main()
         {
-            for (int i = 1; i <= 100; i++)
+            Console.Write("Enter your selection (1, 2, or 3): ");
+            string s = Console.ReadLine();
+            int n = Int32.Parse(s);
+
+            switch (n)
             {
-                if (i == 5)
-                {
+                case 1:
+                    Console.WriteLine("Current value is {0}", 1);
                     break;
-                }
-                Console.WriteLine(i);
+                case 2:
+                    Console.WriteLine("Current value is {0}", 2);
+                    break;
+                case 3:
+                    Console.WriteLine("Current value is {0}", 3);
+                    break;
+                default:
+                    Console.WriteLine("Sorry, invalid selection.");
+                    break;
             }
 
             // Keep the console open in debug mode.
