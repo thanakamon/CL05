37d5f7c9489b1c93b5ad5e9c3d0fa0372d99a9ab
thanakamon <thanakamonmay@gmail.com>
2018-10-02 00:39:07 +0700

    18

--- CL05/CL05/Program.cs
+++ CL05/CL05/Program.cs
@@ -1,39 +1,17 @@
 ﻿using System;
-using System.IO;
-public class switchLearning
+
+namespace ConsoleApp2
 {
-    public static void Main()
+    class WhileTest
     {
-        Console.Write("Input day name : ");
-        string day = Console.ReadLine();
-        string message;
-        switch (day)
+        static void Main()
         {
-            case "sun":
-                message = "sun is Sunday, color Red";
-                break;
-            case "mon":
-                message = "mon is Monday, color Yellow";
-                break;
-            case "tue":
-                message = "tue is Tuesday, color Pink";
-                break;
-            case "wed":
-                message = "wed is Wednesday, color Green";
-                break;
-            case "thu":
-                message = "thu is Thursday, color Orange";
-                break;
-            case "fri":
-                message = "fri is Friday, color Blue";
-                break;
-            case "sat":
-                message = "sat is Saturday, color Purple";
-                break;
-            default:
-                message = "Error";
-                break;
+            int n = 1;
+            while (n < 6)
+            {
+                Console.WriteLine("Current value of n is {0}", n);
+                n++;
+            }
         }
-        Console.WriteLine(message);
     }
 }
