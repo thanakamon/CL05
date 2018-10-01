ce5004624f2fafee3a620502be7331ceacffb96e
thanakamon <thanakamonmay@gmail.com>
2018-10-01 22:08:15 +0700

    1

---
+++ CL05/CL05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class GotoTest
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Line 1");
+        Console.WriteLine("Line 2");
+        Console.WriteLine("Line 3");
+    line4:
+        Console.WriteLine("Line 4");
+        Console.WriteLine("Line 5");
+        Console.WriteLine("Line 6");
+        goto line10;
+        Console.WriteLine("Line 7");
+        Console.WriteLine("Line 8");
+        Console.WriteLine("Line 9");
+    line10:
+        Console.WriteLine("Line 10");
+    }
+}
+
+
+
