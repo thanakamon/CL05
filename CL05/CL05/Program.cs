a618b2d10cd6290f75b26bd18a84b09e099c844d
thanakamon <thanakamonmay@gmail.com>
2018-10-01 22:33:33 +0700

    9

--- CL05/CL05/Program.cs
+++ CL05/CL05/Program.cs
@@ -3,8 +3,9 @@ public class TryCatch
 {
     static void Main(string[] args)
     {
-        int a = int.MaxValue;
-        a *= 2;
+        int a = 0;
+        int b = 10;
+        b /= a;
         Console.WriteLine(a);
     }
 }
