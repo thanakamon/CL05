d9140cc4d3307222119b98afca9db1f7c66b6cf2
thanakamon <thanakamonmay@gmail.com>
2018-10-01 22:29:22 +0700

    8

--- CL05/CL05/Program.cs
+++ CL05/CL05/Program.cs
@@ -3,19 +3,9 @@ public class TryCatch
 {
     static void Main(string[] args)
     {
-        int a = 0;
-        try
-        {
-
-            Console.WriteLine(100 / a);
-        }
-        catch (NullReferenceException e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        catch (DivideByZeroException e)
-        {
-            Console.WriteLine(e.Message);
-        }
+        int a = int.MaxValue;
+        a *= 2;
+        Console.WriteLine(a);
     }
 }
+
