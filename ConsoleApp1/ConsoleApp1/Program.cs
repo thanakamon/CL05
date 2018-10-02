dd3ad8b98d18e17f146a4e2b55744157ad1b81f2
thanakamon <thanakamonmay@gmail.com>
2018-10-02 17:01:25 +0700

    27 ed

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class TryCatch
+{
+    static void Main(string[] args)
+    {
+        int a = 0;
+        int b = 10;
+        b /= a;
+        Console.WriteLine(a);
+    }
+}
